6c0e5050ddd3a124264ba8e9e2abc3296f984374
Khalifa AlMheiri <zunon@zunon.io>
2022-10-19 06:59:48 +0400

    Define structs to be used for project. Parse and allocate the necessary data.

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: kalmheir <kalmheir@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 08:19:06 by kalmheir          #+#    #+#             */
-/*   Updated: 2022/10/18 09:06:04 by kalmheir         ###   ########.fr       */
+/*   Updated: 2022/10/19 06:34:48 by kalmheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,53 @@
 # include <sys/time.h>
 # include <pthread.h>
 # include <stdbool.h>
+
+typedef unsigned long t_milliseconds;
+
+typedef enum e_direction
+{
+	UNDECIDED,
+	LEFT,
+	RIGHT
+}	t_direction;
+
+typedef enum e_philo_state 
+{
+	THINKING,
+	EATING,
+	SLEEPING,
+	DEAD
+}	t_philo_state;
+
+typedef struct s_philo_fork
+{
+	pthread_mutex_t	mutex;
+	bool			interest[2];
+	bool			taken;
+	t_direction		first;
+}	t_philo_fork;
+
+typedef struct s_philosopher
+{
+	t_philo_state	current_state;
+	t_philo_fork	*left_fork;
+	t_philo_fork	*right_fork;
+}	t_philosopher;
+
+typedef struct s_philo_parameters
+{
+	t_milliseconds	starve_time;
+	t_milliseconds	eat_time;
+	t_milliseconds	sleep_time;
+}	t_philo_parameters;
+
+typedef struct s_roundtable
+{
+	t_philo_fork		*forks;
+	t_philosopher		*philosophers;
+	t_philo_parameters	health;
+	unsigned long		*min_eats;
+	size_t				chairs;
+}	t_roundtable;
+
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: kalmheir <kalmheir@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 08:19:15 by kalmheir          #+#    #+#             */
-/*   Updated: 2022/10/18 09:47:47 by kalmheir         ###   ########.fr       */
+/*   Updated: 2022/10/19 06:53:16 by kalmheir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,18 +35,133 @@ bool numeric_args(int argc, char *argv[])
 
 int	panic(char *msg, int errno)
 {
-	printf("%s\n", msg);
+	printf("ERROR: %s\n", msg);
 	return (errno);
+}
+
+bool ul_overflow(unsigned long before, unsigned long after)
+{
+	return (((before >> 63) & 1) && !((after >> 63) & 1));
+}
+
+unsigned long	*atoul(const char *string, unsigned long *location)
+{
+	size_t			i;
+	unsigned long	accumulator;
+	unsigned long	temp;
+
+
+	i = 0;
+	accumulator = 0;
+	while (string[i])
+	{
+		temp = accumulator;
+		accumulator *= 10;
+		accumulator += string[i++] + '0';
+		if (ul_overflow(temp, accumulator))
+			return (NULL);
+	}
+	*location = accumulator;
+	return (location);
+}
+
+int	roundtable_alloc(t_roundtable *table)
+{
+	size_t	i;
+
+	i = -1;
+	table->philosophers = malloc(table->chairs * sizeof(t_philosopher));
+	if (!(table->philosophers))
+		return (1);
+	table->forks = malloc(table->chairs * sizeof(t_philo_fork));
+	if (!(table->forks))
+	{
+		free(table->philosophers);
+		table->philosophers = NULL;
+		return (2);
+	}
+	while (++i < table->chairs)
+	{
+		table->forks[i] = (t_philo_fork){0};
+		table->philosophers[i] = (t_philosopher){THINKING, &(table->forks[i]), &(table->forks[(i + 1) % table->chairs])};
+		if (pthread_mutex_init(&(table->forks->mutex), NULL))
+		{
+			free(table->philosophers);
+			free(table->forks);
+			return (3);
+		}
+	}
+	return (0);
+}
+
+int	roundtable_init(t_roundtable *table, char *arguments[], bool eat_limit)
+{
+	unsigned long	min_eats;
+	size_t			number_of_philosophers;
+	t_milliseconds	time_to_die;
+	t_milliseconds	time_to_eat;
+	t_milliseconds	time_to_sleep;
+
+	if (!atoul(arguments[0], &number_of_philosophers) && number_of_philosophers)
+		return (panic("Can't make that number of chairs!", 1));
+	if (!atoul(arguments[1], &time_to_die))
+		return (1);
+	if (!atoul(arguments[2], &time_to_eat))
+		return (1);
+	if (!atoul(arguments[3], &time_to_sleep))
+		return (1);
+	if (eat_limit && !atoul(arguments[4], &min_eats))
+		return (1);
+	else if (eat_limit)
+		*(table->min_eats) = min_eats;
+	table->chairs = number_of_philosophers;
+	table->health = (t_philo_parameters){time_to_die, time_to_eat, time_to_sleep};
+	if (roundtable_alloc(table))
+		return (2);
+	return (0);
+}
+
+int	roundtable_destroy(t_roundtable *table)
+{
+	free(table->philosophers);
+	free(table->forks);
+	if (table->min_eats)
+		free(table->min_eats);
+	memset(table, 0x00, sizeof(t_roundtable));
+	return (0);
+}
+
+void	simulate_philosophers(t_roundtable *table)
+{
+	printf("World Simulated Successfully!\n");
+	roundtable_destroy(table);
 }
 
 int main(int argc, char *argv[])
 {
+	t_roundtable	table;
+
 	if (argc < 5 || argc > 6)
-		return (panic("ERROR: Incorrect Number of Arguments!", 1));
+		return (panic("Incorrect Number of Arguments!", 1));
 	else if (!numeric_args(argc, argv))
-		return (panic("ERROR: Please Only Use Numerical Arguments!", 2));
+		return (panic("Non-Integer Arguments!", 2));
 	else
 	{
-		return (0);
+		if (argc > 5)
+		{
+			table.min_eats = malloc(sizeof(unsigned long));
+			if (!(table.min_eats))
+				return (panic("Couldn't Create Scenario!", 3));
+		}
+		else
+			table.min_eats = NULL;
+		if (roundtable_init(&table, argv + 1, argc > 5))
+		{
+			if (table.min_eats)
+				free(table.min_eats);
+			return (panic("Couldn't Create Scenario!", 3));
+		}
+		simulate_philosophers(&table);
+		return (roundtable_destroy(&table));
 	}
 }
